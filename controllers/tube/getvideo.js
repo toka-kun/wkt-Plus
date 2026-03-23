7514e5f2ac3eae250f3cf50e82c891b9b00b1c85
Toka_Kun_ <222786419+toka-kun@users.noreply.github.com>
2026-03-23 20:06:12 +0900

    Update getvideo.js

--- controllers/tube/getvideo.js
+++ controllers/tube/getvideo.js
@@ -7,7 +7,7 @@ const axios = require("axios"); // ★ 追加: これがないと負荷チェッ
 const user_agent = process.env.USER_AGENT || "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36";
 
 // サーバーリスト
-const serverUrls = ['invidious', 'siawaseok', 'yudlp', 'xeroxyt-nt-apiv1', 'min-tube2-api', 'simple-yt-stream'];
+const serverUrls = ['invidious', 'siawaseok', 'yudlp', 'ytdlpinstance-vercel', 'min-tube2-api', 'xeroxyt-nt-apiv1', 'simple-yt-stream'];
 
 // ... (上部の設定などはそのまま) ...
 
