a9dcf69b0f5bb336021b7b78f74c6202a288a2a5
Toka_Kun_ <222786419+toka-kun@users.noreply.github.com>
2026-04-30 17:10:02 +0900

    Update game.js

--- routes/game.js
+++ routes/game.js
@@ -6,6 +6,10 @@ router.get("/", (req, res) => {
   res.render("game/home");
 });
 
+router.get("/games", (req, res) => {
+  res.render("game/games/home");
+});
+
 router.get('/games/:id', async (req, res) => {
   const Id = req.params.id;
   res.render(`game/games/${Id}`);
