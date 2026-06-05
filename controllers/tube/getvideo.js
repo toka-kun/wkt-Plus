c93f9d5f72fe907483c8c5a3d67da66c053772ee
Toka_Kun_ <222786419+toka-kun@users.noreply.github.com>
2026-06-05 21:34:16 +0900

    Add 'freemake' to serverUrls array

--- controllers/tube/getvideo.js
+++ controllers/tube/getvideo.js
@@ -7,7 +7,7 @@ const axios = require("axios");
 const user_agent = process.env.USER_AGENT || "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36";
 
 // サーバーリスト
-const serverUrls = ['invidious', 'acethinker', 'siawaseok', 'yudlp', 'ytdlpinstance-vercel', 'senninytdlp', 'min-tube2-api', 'xeroxyt-nt-apiv1', 'simple-yt-stream'];
+const serverUrls = ['invidious', 'acethinker', 'siawaseok', 'yudlp', 'ytdlpinstance-vercel', 'senninytdlp', 'min-tube2-api', 'xeroxyt-nt-apiv1', 'simple-yt-stream', 'freemake'];
 
 router.get('/:id', async (req, res) => {
     const videoId = req.params.id;
